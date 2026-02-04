f978223f84237b679655c48075a64101383ad28d
Prasann Barot <43604788+prasannbarot@users.noreply.github.com>
2026-02-04 13:38:17 -0500

    Implement twoSum method in Solution class

--- 1. Two Sum/Solution.py
+++ 1. Two Sum/Solution.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i , x in enumerate(nums):
+            y = target - x
+            if y in seen:
+                return[seen[y],i]
+            seen[x]=i
+        return []
