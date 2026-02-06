0be083cbfc089910be813332e139b9d65d4040dc
Prasann Barot <43604788+prasannbarot@users.noreply.github.com>
2026-02-06 11:59:08 -0500

    Implement Roman to Integer conversion

---
+++ 13. Roman to Integer/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        ans = 0
+        bank = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+        for a,b in zip(s, s[1:]):
+            if bank[a] < bank[b]:
+                ans -= bank[a]
+            else :
+                ans += bank[a]
+        return ans + bank[s[-1]]
